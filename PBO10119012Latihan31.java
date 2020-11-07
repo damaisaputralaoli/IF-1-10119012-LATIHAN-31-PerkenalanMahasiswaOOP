6d406a618d6e837ffc607e07009920b44b6017b8
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-07 18:00:34 +0700

    Add files via upload

---
+++ PBO10119012Latihan31.java
@@ -0,0 +1,34 @@
+
+package pbo10119012latihan31;
+
+
+/**
+ *
+ * @author damai
+ * NAMA   : DAMAI SAPUTRA LAOLI
+ * KELAS  : IF-1
+ * NIM    : 10119012
+ */
+public class PBO10119012Latihan31 {
+
+    
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa();
+        Mahasiswa mhs2 = new Mahasiswa();
+        Mahasiswa mhs3 = new Mahasiswa();
+        Mahasiswa mhs4 = new Mahasiswa();
+        mhs1.nim = "10110269";
+        mhs1.nama = "Rizki Adam Kurniawan";
+        mhs2.nim = "10112270";
+        mhs2.nama = "Indra Tiola";
+        mhs3.nim = "10110271";
+        mhs3.nama = "Robi Tansil Ganefi";
+        mhs4.nim = "10110269";
+        mhs4.nama = "Muhammad Nur Awaludin";
+        mhs1.pekenalkanDiri();
+        mhs2.pekenalkanDiri();
+        mhs3.pekenalkanDiri();
+        mhs4.pekenalkanDiri();
+    }
+}
+
